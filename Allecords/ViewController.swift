4d0b7d34ced827c09253e6e951818f9dc1a99924
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-03-31 00:30:00 +0900

    Project Setting

---
+++ Allecords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Allecords
+//
+//  Created by Hoon on 3/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
