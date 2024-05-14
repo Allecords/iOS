40e7f596ea1fa95f9e3b455b40fdd39c104add34
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-05-14 20:00:31 +0900

    fix: Cache 중첩 로직 삭제

--- Allecords/Allecords/Utils/AllecordsImage/Downloader/AllecordsImageLoader.swift
+++ Allecords/Allecords/Utils/AllecordsImage/Downloader/AllecordsImageLoader.swift
@@ -13,54 +13,42 @@ final actor AllecordsImageLoader: AllecordsImageLoadable {
 		case complete(UIImage)
 	}
 	
-	private var cache: [String: DownloadEntry] = [:]
-	private var count: [String: Int] = [:]
+	private var tasks: [String: DownloadEntry] = [:]
 	
 	/// URL 이미지 다운로드
 	/// - Parameters:
 	///   - url: 이미지 URL:
 	/// - Returns: 이미지 다운로드 결과
 	func downloadImage(from url: String) async throws -> UIImage {
-		count[url, default: 0] = 1
-		defer {
-			if count[url] != nil {
-				count[url]! -= 1
-				if count[url]! <= 0 {
-					cache[url] = nil
-					count[url] = nil
-				}
-			}
-		}
-		
-		if let cached = cache[url] {
-			switch cached {
-			case .inProgress(let task):
-				return try await task.value
-			case .complete(let imageData):
-				return imageData
-			}
+		switch tasks[url] {
+		case .some(.complete(let image)):
+			return image
+		case .some(.inProgress(let task)):
+			return try await task.value
+		case .none:
+			break
 		}
 		
 		let task = Task {
 			try await download(from: url)
 		}
 		
-		cache[url] = .inProgress(task)
+		tasks[url] = .inProgress(task)
 		
 		do {
 			let imageData = try await task.value
-			cache[url] = .complete(imageData)
+			tasks[url] = .complete(imageData)
 			return imageData
 		} catch {
-			cache[url] = nil
+			tasks[url] = nil
 			throw error
 		}
 	}
 	
 	func cancelDownloadImage(url: String) async {
-		guard let cached = cache[url] else { return }
+		guard let task = tasks[url] else { return }
 		
-		switch cached {
+		switch task {
 		case .inProgress(let task):
 			if !task.isCancelled {
 				task.cancel()
