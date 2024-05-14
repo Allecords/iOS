ad9b6564f174084cac49f4fdc957e96b591265a4
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-05-14 16:29:18 +0900

    feat: UIImageView + Loader Extension

--- Allecords/Allecords/Extension/UIKit/UIImageView+.swift
+++ Allecords/Allecords/Extension/UIKit/UIImageView+.swift
@@ -5,4 +5,28 @@
 //  Created by Hoon on 5/14/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+	func setImage(from urlString: String) {
+		Task { [weak self] in
+			do {
+				let image = try await AllecordsImageLoader.shared.downloadImage(from: urlString)
+				DispatchQueue.main.async {
+					self?.image = image
+				}
+			} catch {
+				debugPrint("Failed to download image: \(error)")
+				DispatchQueue.main.async {
+					self?.image = nil
+				}
+			}
+		}
+	}
+	
+	func cancelImageDownload(for urlString: String) {
+		Task {
+			await AllecordsImageLoader.shared.cancelDownloadImage(url: urlString)
+		}
+	}
+}
