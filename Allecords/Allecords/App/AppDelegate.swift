18ca67d9bde25c743a857816f20e8190c8add147
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-05-15 16:57:06 +0900

    chore: develop 기준 업데이트, AppDelegate 불필요 메서드 삭제

--- Allecords/Allecords/App/AppDelegate.swift
+++ Allecords/Allecords/App/AppDelegate.swift
@@ -15,12 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	) -> Bool {
 		return true
 	}
-
-	func application(
-		_ application: UIApplication,
-		configurationForConnecting connectingSceneSession: UISceneSession,
-		options: UIScene.ConnectionOptions
-	) -> UISceneConfiguration {
-		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-	}
 }
